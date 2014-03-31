d5b31bcb3f653c3db91f0662222259514f71e943
CodeGood <aparajita92@gmail.com>
2014-03-31 21:50:48 +0530

    No Nag Scheduler is done. Choosing from contacts also works. I'm not sure if we should burden the user expecting him to remember a number by heart to enter it manually. :P

--- src/com/appointext/frontend/NoNagMode.java
+++ src/com/appointext/frontend/NoNagMode.java
@@ -3,7 +3,9 @@ package com.appointext.frontend;
 import com.bmsce.appointext.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NoNagMode extends Activity {
 	
@@ -12,6 +14,14 @@ public class NoNagMode extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.no_nag);
 	}
+	
+	public void NoNagSchedule(View V)	{
+		Intent intent = new Intent(this, NoNagScheduler.class);
+		startActivity(intent); 
+			
+	}
+	
+	
 	
 	
 }
