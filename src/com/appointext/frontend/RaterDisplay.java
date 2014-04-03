d7c630353ebf172aa64d7ae6914f3a231d95d510
CodeGood <aparajita92@gmail.com>
2014-04-03 15:52:31 +0530

    Settings have been changed to immediately reflect the changes. :P

--- src/com/appointext/frontend/RaterDisplay.java
+++ src/com/appointext/frontend/RaterDisplay.java
@@ -12,6 +12,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RaterDisplay extends Activity {
 	
@@ -25,37 +26,15 @@ public class RaterDisplay extends Activity {
 		Log.i("Display", "Reached Rater Method");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.rate);
-		
-	/*	RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
-		Log.i("AppoinText", "You rated me at " + String.valueOf(rb.getRating()));*/
-		//addListenerOnRatingBar();
 		addListenerOnButton();
 	}
 
-	/*public void addListenerOnRatingBar() {
-		 
-		ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-		txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
-	 
-		//if rating value is changed,
-		//display the current rating value in the result (textview) automatically
-		ratingBar.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
-			public void onRatingChanged(RatingBar ratingBar, float rating,
-				boolean fromUser) {
-	 
-				txtRatingValue.setText(String.valueOf(rating));
-	 
-			}
-		});
-	  }*/
 	 
 	  public void addListenerOnButton() {
 		  
 		txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
 		ratingBar = (RatingBar) findViewById(R.id.ratingBar);
 		btnSubmit = (Button) findViewById(R.id.btnSubmit);
-	 
-		//if click on me, then display the current rating value.
 		btnSubmit.setOnClickListener(new OnClickListener() {
 	 
 			@Override
