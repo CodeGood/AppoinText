1a9d74c630c830d4d998b55666d1a55d092b4698
aparajita92 <aparajita92@gmail.com>
2014-04-04 01:38:31 +0530

    Removed few bugs from RecognizeDay

--- src/com/appointext/regex/RecognizeDay.java
+++ src/com/appointext/regex/RecognizeDay.java
@@ -8,6 +8,18 @@ import java.util.Calendar;
 public class RecognizeDay {
 
 	private static Calendar c = Calendar.getInstance(); //Got the current date
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //to parse dates
+
+	public static void main(String[] args)throws Exception {
+
+System.out.println("SMS = '" + args[0] + "'\tDate = " + (args.length > 1 ? args[1] : sdf.format(c.getTime())));
+	
+		if (args.length > 1) { //If you did indeed try to set the day
+			c.setTime(sdf.parse(args[1]));
+		}
+	
+		System.out.println("Found days of Week as : " + findDay(args[0]) + "\n");
+	}
 
 	/**
 	 * Returns a String of Days (of the week) found, and AN EMPTY STRING (NOT equal to null) if none are found
@@ -25,9 +37,16 @@ public class RecognizeDay {
 		
 			for (int j = 0; j < dotw.length; j++) {			
 			
-				if (dotw[j].equals(words[i]) && !(words[i-1].equalsIgnoreCase("last"))) //am not interested in 'past' dates!
-					foundDates += convertToDate((j > 6 ? j-7 : j), words[i-1]);
-					foundDates += ",";
+				if (dotw[j].equals(words[i])) {
+					if (i != 0 ) {
+						if (!(words[i-1].equalsIgnoreCase("last"))) { //am not interested in 'past' dates!
+						
+							foundDates += convertToDate((j > 6 ? j-7 : j), words[i-1], (i > 3 ? (words[i-3]+words[i-2]) : null));
+							foundDates += ",";
+							
+						}
+					}
+				}
 			}
 			
 		}
@@ -35,7 +54,7 @@ public class RecognizeDay {
 		return foundDates.substring(0, foundDates.length()-1);
 	 }
 	 
-	 public static String convertToDate(int day, String prevWord) {
+	 public static String convertToDate(int day, String prevWord, String beforePrev) {
 
 		int today = c.get(Calendar.DAY_OF_WEEK); //Got the day out of it.
 		
@@ -52,8 +71,17 @@ public class RecognizeDay {
 			if (toAdd < 0) //Perform the modulo 7 thing
 				toAdd = 7 + toAdd;
 				
-			if (day + toAdd <= 7) //and generally next week means, AFTER sunday, so add 7 to it.
+/*
+THIS APPROACH SEEMS TOO BUGGY TO BOTHER WITH MORE. LETS FIND THE NEXT SUNDAY AND CHECK.				
+System.out.println("ToDay + toAdd is " + (today+toAdd));				
+			if (today+toAdd > 7) //and generally next week means, AFTER sunday, so add 7 to it.
 					toAdd += 7;
+*/
+			int addToGetToNextSunday = 8-today; //this is the number to be added to get to the next Sunday (Rember the guy needs 1 extra).
+
+			if (toAdd <= addToGetToNextSunday)
+				toAdd += 7;
+
 		
 		}
 		else { //Today is Sunday. I say Wednesday. 3 - 0 = 3. Today is Sunday, I say Thursday. 4 - 0 = 4
@@ -63,8 +91,10 @@ public class RecognizeDay {
 				toAdd = 7 + toAdd;
 		}
 		
+		if (beforePrev != null && beforePrev.equalsIgnoreCase("nextto")) //Deal with next to next :P
+			toAdd += 7;
+		
 		c.add(Calendar.DATE, toAdd+1); //Okay looks like it needs one extra
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-		return formatter.format(c.getTime());
+		return sdf.format(c.getTime());
 	 }
 }
