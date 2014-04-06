c91f162fa6c98fb30d299958fe180f39b6434530
CodeGood <aparajita92@gmail.com>
2014-04-06 23:48:42 +0530

    The excluded numbers looks a lot more aesthetically pleasing now. :P
    And shortly shall include the ability to delete numbers as well. B)

--- src/com/appointext/frontend/BlockedNumberDisplay.java
+++ src/com/appointext/frontend/BlockedNumberDisplay.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 import com.appointext.database.DatabaseManager;
 import com.bmsce.appointext.R;
-
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class BlockedNumberDisplay extends Activity {
 
@@ -15,16 +18,38 @@ public class BlockedNumberDisplay extends Activity {
 	protected void onCreate(Bundle savedInstanceState) { //Demo
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.blockednumberdisplay);
-		TextView text = (TextView) findViewById(R.id.blockedNumberDisplayer);
+		LinearLayout linear = (LinearLayout)findViewById(R.id.linearLayout);
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+	            LinearLayout.LayoutParams.MATCH_PARENT,
+	            LinearLayout.LayoutParams.WRAP_CONTENT);
 		DatabaseManager db = new DatabaseManager(this);
 		db.open();
 		ArrayList<Object> row;
 		row = db.getRowAsArray("settingsTable", "BlockedNumbers");
-		if(row.isEmpty())
-			text.setText("No numbers are excluded from AppoinText's jurisdiction");
-		else	{
-			String numbers = row.get(1).toString().replaceAll(",","\n");
-			text.setText(numbers);
+		if(row.isEmpty())	{
+			TextView text = new TextView(this);
+			text.setText("No Numbers Have Been Excluded!");
 		}
+		else	{
+			int i=0;
+			String[] numbers = row.get(1).toString().split(",");
+			//.setText(numbers);
+			for(String number : numbers)	{
+				i++;
+			    Button btn = new Button(this);
+			    btn.setId(i);
+			    final int id_ = btn.getId();
+			    btn.setText(number);
+			    linear.addView(btn, params);
+			    Button btn1 = ((Button) findViewById(id_));
+			    btn1.setOnClickListener(new View.OnClickListener() {
+			        public void onClick(View view) {
+			            Toast.makeText(view.getContext(),
+			                    "Button clicked index = " + id_, Toast.LENGTH_SHORT)
+			                    .show();
+			}
+		});
+	}
+}
 	}
 }
