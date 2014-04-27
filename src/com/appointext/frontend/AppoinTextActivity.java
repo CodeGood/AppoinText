98938ee41b788a45de6dfcb3ebed248bf47a01eb
Ashwin S <ashwin.terminator@gmail.com>
2014-04-27 20:06:09 +0530

    Update AppoinTextActivity.java

--- src/com/appointext/frontend/AppoinTextActivity.java
+++ src/com/appointext/frontend/AppoinTextActivity.java
@@ -75,13 +75,18 @@ Log.i("AT MainActivity", "Registering Content Resolver.");
     
 	public void viewAbout(View v) {
     	AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("AppoinText v2 ®™");
+        alertDialog.setTitle("AppoinText v2 Â®â„¢");
         alertDialog.setMessage("Developed by Abhishek Kasargod, Aparajita Raychaudhury, Ashwin S and Manasa G.\n 2013-2014 Project at BMSCE\nUnder guidance of Ms. Saritha AN");
         alertDialog.setButton (DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                 }
         });
         alertDialog.show();
+    }
+	
+	public void viewHelp(View v) {
+    	Intent intent = new Intent(this, HelpDisplay.class);
+		startActivity(intent); 
     }
 
 }
