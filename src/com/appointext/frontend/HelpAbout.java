be2ada624d0d985ec57cf5c82cf0c826fdbc4f32
Ashwin S <ashwin.terminator@gmail.com>
2014-04-29 14:51:34 +0530

    Ironed out some bugs that bugged the Help thing.

--- src/com/appointext/frontend/HelpAbout.java
+++ src/com/appointext/frontend/HelpAbout.java
@@ -14,7 +14,7 @@ public class HelpAbout extends Activity {
 		setContentView(R.layout.helpabout);
 		TextView text = (TextView)findViewById(R.id.helpAbout);
 		text.setText("Welcome to Appointext!\n\n" +
-					 "AppoinText is a simple(to use, not to develop. :P) application that understands the reads your messages and auto generates reminders if it find that a reminder will be necessary");
+					 "AppoinText is a simple to use(not to develop. :P) application that reads your messages and auto generates reminders if it finds that a reminder will be necessary");
 		text.setShadowLayer(5, 0, 0, Color.BLACK);
 		
 	}
