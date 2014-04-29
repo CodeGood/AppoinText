510de2d483060e3fd5d47b7633d10e74d5d0a0c5
CodeGood <aparajita92@gmail.com>
2014-04-29 14:05:41 +0530

    All done

--- src/com/appointext/frontend/AppoinTextActivity.java
+++ src/com/appointext/frontend/AppoinTextActivity.java
@@ -1,4 +1,4 @@
-package com.appointext.frontend; 
+package com.appointext.frontend;
 //import android.app.ActionBar;
 
 
@@ -6,87 +6,83 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.CalendarContract.Events;
 import android.util.Log;
 import android.view.View;
 
-import com.appointext.backend.SMSListenerSent;
-import com.appointext.database.CalendarInsertEvent;
-import com.appointext.database.GetCalendarEvents;
 import com.bmsce.appointext.R;
 //import android.view.Menu;
 //import android.view.MenuItem;
 //import android.view.MenuInflater;
 
 
-public class AppoinTextActivity extends Activity  {
+public class AppoinTextActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		Intent i = new Intent(this, com.appointext.backend.MyUpdateService.class);
 		startService(i);
-Log.i("AppoinTextReminder", "Started Service");
-Log.i("AppoinTextReminder", "Result : " + GetCalendarEvents.getEventByID(this, "3", new String[] {Events.DTSTART} ));
-Log.i("AT MainActivity", "Registering Content Resolver.");
 
-		final Uri SMS_STATUS_URI = Uri.parse("content://sms");
-		SMSListenerSent smsSentObserver = new SMSListenerSent(new Handler(), this);
-		this.getContentResolver().registerContentObserver(SMS_STATUS_URI, true, smsSentObserver);
+		Log.i("AppoinTextReminder", "Started Service");
+		Log.i("AT MainActivity", "Registering Content Resolver.");
 
+		/*
+final Uri SMS_STATUS_URI = Uri.parse("content://sms");
+SMSListenerSent smsSentObserver = new SMSListenerSent(new Handler(), this);
+this.getContentResolver().registerContentObserver(SMS_STATUS_URI, true, smsSentObserver);
+		 */
 
 	}
 
 	public void viewSettings(View V) {
 		Intent intent = new Intent(this, SettingsDisplay.class);
-		startActivity(intent);        
+		startActivity(intent);
 	}
 
 	public void viewReminders(View v) {
 		Intent intent = new Intent(this, RemindersDisplay.class);
-		startActivity(intent); 
-    }
+		startActivity(intent);
+	}
 
 	public void viewInviter(View v) {
 		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
-	    shareIntent.setType("text/plain");
-	    shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Try AppoinText for Android!");
-	    shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I'm using AppoinText for Android and I recommend it! Go get it on the Play Store! :)");
+		shareIntent.setType("text/plain");
+		shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Try AppoinText for Android!");
+		shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I'm using AppoinText for Android and I recommend it! Go get it on the Play Store! :)");
 
-	    Intent chooserIntent = Intent.createChooser(shareIntent, "Share with");
-	    chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-	    startActivity(chooserIntent);   
-    }
-    
-    public void viewRater(View v) {
-    	Intent intent = new Intent(this, RaterDisplay.class);
-		startActivity(intent); 
-    }
-    
-    public void viewTnc(View v) {
-    	Intent intent = new Intent(this, TNCDisplay.class);
-		startActivity(intent); 
-    }
-    
+		Intent chooserIntent = Intent.createChooser(shareIntent, "Share with");
+		chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(chooserIntent);
+	}
+
+	public void viewRater(View v) {
+		Intent intent = new Intent(this, RaterDisplay.class);
+		startActivity(intent);
+	}
+
+	public void viewTnc(View v) {
+		Intent intent = new Intent(this, TNCDisplay.class);
+		startActivity(intent);
+	}
+
 	public void viewAbout(View v) {
-    	AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("AppoinText v2 Â®â„¢");
-        alertDialog.setMessage("Developed by Abhishek Kasargod, Aparajita Raychaudhury, Ashwin S and Manasa G.\n 2013-2014 Project at BMSCE\nUnder guidance of Ms. Saritha AN");
-        alertDialog.setButton (DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                }
-        });
-        alertDialog.show();
-    }
-	
-	/*public void viewHelp(View v) {
-    	Intent intent = new Intent(this, HelpDisplay.class);
-		startActivity(intent); 
-    }*/
+		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+		alertDialog.setTitle("AppoinText v2 ®™");
+		alertDialog.setMessage("Developed by Abhishek Kasargod, Aparajita Raychaudhury, Ashwin S and Manasa G.\n 2013-2014 Project at BMSCE\nUnder guidance of Ms. Saritha AN");
+		alertDialog.setButton (DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+		alertDialog.show();
+	}
+
+	public void viewHelp(View v) {
+		Intent intent = new Intent(this, HelpDisplay.class);
+		startActivity(intent);
+	}
 
 }
