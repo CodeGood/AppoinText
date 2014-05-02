ba6ee485fd47b709b33b2b9bd8ef9be60a262f2b
aparajita92 <aparajita92@gmail.com>
2014-05-02 23:10:53 +0530

    Ironed out issues with RecognizeTime. Or so I hope.

--- src/com/appointext/database/DatabaseManager.java
+++ src/com/appointext/database/DatabaseManager.java
@@ -1,6 +1,8 @@
 package com.appointext.database;
  
 import java.util.ArrayList;
+import java.util.Calendar;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -8,9 +10,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
  
 public class DatabaseManager {
 
